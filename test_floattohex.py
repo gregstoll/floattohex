56fbe5c450b57c47596bf59e9f6c57da927c1e78
Greg Stoll <greg@gregstoll.com>
2022-09-28 20:03:24 -0500

    setup for testing

---
+++ test_floattohex.py
@@ -0,0 +1,9 @@
+import floattohex
+import unittest
+
+class Test_FloatToHexParameterized(unittest.TestCase):
+    def test_fileFloatToHex(self):
+        self.assertEqual(1, 1)
+
+if __name__ == '__main__':
+    unittest.main()
