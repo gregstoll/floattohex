e85eb7e37d1699250fc92a0da7ade9cb0b52bbc7
Greg Stoll <greg@gregstoll.com>
2025-01-01 21:36:20 -0600

    cgi_server (not tested though)

--- floattohexcgi-rust/src/bin/cgi_server.rs
+++ floattohexcgi-rust/src/bin/cgi_server.rs
@@ -1,4 +1,59 @@
+use std::collections::HashMap;
 
-fn main() {
-    println!("Hello, world!");
+use cgi::http;
+
+extern crate cgi;
+
+fn error(s: &str) -> cgi::Response {
+    cgi::binary_response(200, "text/xml", format!("Error: {}", s).into_bytes())
 }
+
+fn success(s: &str) -> cgi::Response {
+    cgi::binary_response(200, "text/xml", s.as_bytes().into())
+}
+
+fn get_query_value<'a>(
+    query_parts: &'a HashMap<String, String>,
+    key: &str,
+) -> Result<&'a str, String> {
+    query_parts
+        .get(key)
+        .map(|x| x.as_str())
+        .ok_or(format!("Internal error - no {} specified!", key))
+}
+
+fn process_request(request: &cgi::Request) -> Result<String, String> {
+    let query = request
+        .uri()
+        .query()
+        .ok_or(String::from("Internal error - no query string?"))?;
+    // this is inefficient, can make this a HashMap<&str, &str> or something?
+    let query_parts: HashMap<String, String> = url::form_urlencoded::parse(query.as_bytes())
+        .into_owned()
+        .collect();
+    let action = get_query_value(&query_parts, "action")?;
+    let swap = get_query_value(&query_parts, "swap")? == "1";
+    let is_to_float = action.starts_with("hexto");
+    if is_to_float {
+        let hex_value = get_query_value(&query_parts, "hex")?;
+        Ok(handle_cgi(&action, "", &hex_value, swap))
+    } else {
+        // take off the "tohex" at the end
+        let float_value = get_query_value(&query_parts, &action[..action.len() - 5])?;
+        Ok(handle_cgi(&action, &float_value, "", swap))
+    }
+}
+
+pub fn handle_cgi(action: &str, float_str: &str, hex_str: &str, swap: bool) -> String {
+    floattohexlib::handle_cgi(action, float_str, hex_str, swap)
+}
+
+cgi::cgi_main! { |request: cgi::Request| {
+    let result = process_request(&request);
+    let mut response = match result {
+        Ok(val) => success(&val),
+        Err(err) => error(&err)
+    };
+    response.headers_mut().insert(http::header::ACCESS_CONTROL_ALLOW_ORIGIN, "*".parse().unwrap());
+    response
+} }
